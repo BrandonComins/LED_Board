291afc54c74c4917887ce7921648ca691c89ef1d
BrandComin <brandcomin@gmail.com>
2023-08-24 15:23:26 -0700

    add clear and save button functionality, also change grid to open at resolution/ 2

--- Draw Gui.py
+++ Draw Gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import colorchooser
+import pyautogui as gui
 
 class LEDButton:
     def __init__(self, row : int, colunm : int) -> None:
@@ -11,7 +12,7 @@ class LEDButton:
             window, 
             compound = tk.LEFT,
             bg = self.color,
-            command=self.changeColor,
+            command=self.setColor,
             height= 3,
             width= 1
         )
@@ -20,10 +21,15 @@ class LEDButton:
         window.columnconfigure(self.colunm, weight=1)
         window.rowconfigure(self.row, weight=1)
 
-    def changeColor(self) -> None:
+    def setColor(self) -> None:
         global color_code
         self.button.configure(bg = color_code[-1])
+    
+    def resetColor(self) -> None:
+        self.button.configure(bg = '#ffffff')
 
+    def getColor(self) -> str:
+        return self.button.cget('bg')
 
 
 def chooseColor() -> tuple:
@@ -33,10 +39,48 @@ def chooseColor() -> tuple:
     return color_code
 
 def clearCanvas() -> None:
-    pass
+    for i in range(row):
+        for j in range(column):
+            colorButtonList[i][j].resetColor()
 
 def saveImage() -> None:
-    pass
+    def saveText(event=None) -> None:
+        if(event == None): # For save box
+            text : str = 'Saves/' + inputText.get("1.0","end-1c") + '.txt'
+        else: # For Enter Key
+            text : str = 'Saves/' + inputText.get("1.0","end-2c") + '.txt'
+        
+        questionBox.destroy()
+        
+        file = open(text, "w")
+        for i in range(row):
+            for j in range(column):
+                file.write(colorButtonList[i][j].getColor())
+                if(not (i == row - 1 and j == column - 1)):
+                    file.write(', ')
+            file.write('\n')
+    
+        
+        
+    questionBox = tk.Toplevel(window)
+    questionBox.title("Save Image")
+    questionBox.geometry(str(("500x50+%d+%d" % (int(screenWidth/2.5), int(screenHeight/3)))))
+    questionBox.attributes('-topmost',True)
+    questionBox.bind('<Return>', saveText)
+
+    inputText = tk.Text(questionBox, height = 1,
+                width = 25,
+                bg = "light yellow")
+    
+    inputText.pack(side= 'left', pady = 10, padx = 10)
+
+    tk.Label(questionBox, text=".txt").pack(side = 'left', pady = 10, padx = 10)
+    
+    saveButton = tk.Button(questionBox, height = 2,
+                 width = 10,
+                 text ="save",
+                 command = saveText)
+    saveButton.pack(pady=1)
 
 def loadImage() -> None:
     pass
@@ -47,6 +91,7 @@ def makeColorButton() -> None:
         text="RGB",
         command = chooseColor
     )
+
     window.rowconfigure(row + 1, weight=0)
     color_chooser.grid(sticky="nswe", row= row + 2, column=round(column/2))
 
@@ -80,22 +125,28 @@ def makeSaveButton() -> None:
     
     window.rowconfigure(row + 1, weight=0)
     save.grid(sticky="nswe", row= row + 2, column=column-1)
-
-
+    
+    
 if __name__== "__main__":
     color_code = ((255, 255, 255), '#ffffff') # White
-    row : int = int(input("Row Size: "))
-    column : int = int(input("Column Size: "))
+    # row : int = int(input("Row Size: ")) 
+    # column : int = int(input("Column Size: "))
+    row : int = 10 
+    column : int = 10
 
+    screenWidth, screenHeight = gui.size()
     window = tk.Tk()
-    window.geometry("300x300+960+540")
+    window.geometry(str(("500x500+%d+%d" % (int(screenWidth/2.5), int(screenHeight/3)))))
     window.attributes('-topmost', 1)
+    window.title("Color Grid")
     
-
+    colorButtonList = []
     for i in range(row):
+        colorButtonList.append([])
         for j in range(column):
-            LEDButton(i, j)
-
+            Button = LEDButton(i, j)
+            colorButtonList[i].append(Button)
+    
     makeColorButton()
     makeClearButton()
     makeSaveButton()
