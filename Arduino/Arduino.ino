9d235ce54b57728d858e1af1688fa8d0b5c9b6f2
BrandComin <brandcomin@gmail.com>
2023-08-30 15:42:25 -0700

    split incoming data

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+	Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  if(Serial.available()){
+    String input = Serial.readString();
+    static int * valuesArr = split(input, ' ');
+      
+    int position = valuesArr[0];
+    int red = valuesArr[1];
+    int green = valuesArr[2];
+    int blue = valuesArr[3];
 
+    Serial.println(position);
+    Serial.println(red);
+    Serial.println(blue);
+    Serial.println(green);
+  }
 }
+
+static int * split(String str, const char split){
+  int index = 0;
+  Serial.println(str);
+  static int tempArr[4] = {-1, -1, -1, -1};
+  String temp = "";
+  
+  for(int i = 0; i <= str.length(); ++i){
+    if(str[i] == split || str[i] == '\0'){
+      tempArr[index] = temp.toInt();
+      ++index;
+      temp = "";	
+    }else{
+      temp += str[i];
+    }
+  }
+  return tempArr;
+}
+
