2945686a4d562688c1f3448a30ba1e5c218ff386
BrandComin <brandcomin@gmail.com>
2023-08-31 12:12:25 -0700

    add LED functionality

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,39 +1,59 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 5
+#define LED_COUNT 100
+
+int colorArr[4] = {-1, -1, -1, -1};
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
 void setup(){
-	Serial.begin(9600);
+  strip.begin();           // Initialize NeoPixel object
+  strip.setBrightness(255); // Set BRIGHTNESS to about 4% (max = 255)
+  strip.show();            // Initialize all pixels to 'off'
+  Serial.begin(9600);
 }
 
-void loop(){
+void loop()
+{
   if(Serial.available()){
     String input = Serial.readString();
-    static int * valuesArr = split(input, ' ');
-      
-    int position = valuesArr[0];
-    int red = valuesArr[1];
-    int green = valuesArr[2];
-    int blue = valuesArr[3];
+   	split(input, ' ');
 
-    Serial.println(position);
-    Serial.println(red);
-    Serial.println(blue);
-    Serial.println(green);
+  	strip.setPixelColor(
+      colorArr[0],
+      colorArr[1],
+      colorArr[2],
+      colorArr[3]
+    );
+    strip.show();
+
   }
+  //rainbow(5);
+  //delay(10);
+  //strip.clear();
 }
 
-static int * split(String str, const char split){
+void split(String str, const char split){
   int index = 0;
   Serial.println(str);
-  static int tempArr[4] = {-1, -1, -1, -1};
   String temp = "";
   
   for(int i = 0; i <= str.length(); ++i){
     if(str[i] == split || str[i] == '\0'){
-      tempArr[index] = temp.toInt();
-      ++index;
-      temp = "";	
+      	colorArr[index] = temp.toInt();
+      	++index;
+      	temp = "";	
     }else{
       temp += str[i];
     }
   }
-  return tempArr;
 }
 
+void rainbow(int wait) {
+  for(long firstPixelHue = 0; firstPixelHue < 256*100; firstPixelHue += 256) {
+    strip.rainbow(firstPixelHue);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
