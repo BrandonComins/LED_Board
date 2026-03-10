b62119eb8db66283cd66149ccdc27502c0d8f177
Brandon Comins <BrandComin@gmail.com>
2026-03-09 20:18:10 -0700

    slightly clean

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,11 +1,17 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 5
-#define LED_COUNT 100
+constexpr int led_pin = 5;
+constexpr int led_count = 100;
+const bool debug = false;
 
-int colorArr[4] = {-1, -1, -1, -1};
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+int colorArr[4] = {-1, -1, -1, -1}; // LED #, R, G, B,
+Adafruit_NeoPixel strip(led_count, led_pin, NEO_GRB + NEO_KHZ800);
 
+void test_LEDS() {
+  rainbow(5);
+  delay(10);
+  strip.clear();
+}
 
 void setup(){
   strip.begin();           // Initialize NeoPixel object
@@ -16,24 +22,36 @@ void setup(){
 
 void loop()
 {
-  if(Serial.available()){
-    String input = Serial.readString();
-   	split(input, ' ');
+  if(debug) {
+    if(Serial.available()){
+      String input = Serial.readString();
+      split(input, ' ');
 
-  	strip.setPixelColor(
-      colorArr[0],
-      colorArr[1],
-      colorArr[2],
-      colorArr[3]
-    );
-    strip.show();
-
+      strip.setPixelColor(
+        colorArr[0],
+        colorArr[1],
+        colorArr[2],
+        colorArr[3]
+      );
+      strip.show();
+    }
+  }else{
+      test_LEDS();
   }
-  //rainbow(5);
-  //delay(10);
-  //strip.clear();
 }
 
+/**
+ * Splits a delimited string into integers and stores them in a global array.
+ * * This function iterates through a string, parses segments separated by a 
+ * specific character (e.g., a comma), converts those segments to integers, 
+ * and populates the global 'colorArr' array with the results.
+ *
+ * @param str   The input String containing delimited numeric values.
+ * @param split The delimiter character used to separate values (e.g., ',' or ':').
+ * * @note This function assumes 'colorArr' is large enough to hold all parsed values.
+ * @warning Relies on the global variable 'colorArr'. Be careful of buffer overflows
+ * if the input string contains more segments than the array size.
+ */
 void split(String str, const char split){
   int index = 0;
   Serial.println(str);
